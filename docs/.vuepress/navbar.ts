0cb92b8f6eebac94591014d74125b077282db2cb
rand777 <losmosga@foxmail.com>
2025-02-15 06:28:45 +0800

    docs: add nav icons and certain contents

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,23 +2,24 @@ import {defineNavbarConfig} from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
     {text: '首页', link: '/'},
-    {text: '博客', link: '/blog/'},
-    {text: 'wiki', link: 'https://wiki.macrodd.online/'},
+    {icon: '/blog.svg', text: '博客', link: '/blog/'},
+    {icon: 'vscode-icons:file-type-mediawiki', text: 'wiki', link: 'https://wiki.macrodd.online/'},
     {
+        icon: '/guidance.svg',
         text: '指南',
         items: [
-            {text: '加入我们', link: '/guide/join-us/'},
-            {text: 'Passport配置', link: '/guide/passport-setup/'},
+            {icon: '/join.svg', text: '加入我们', link: '/guide/join-us/'},
+            {icon: 'icon-park:config', text: 'Passport配置', link: '/guide/passport-setup/'},
         ]
     },
     {
         text: '联盟',
         items: [
-            {text: 'MUA', link: 'http://www.mualliance.cn/'},
-            {text: 'Passport', link: 'https://p.mpsmc.cn/'},
-            {text: '服务器中心', link: 'https://server.cqmua.cn/'},
-            {text: '语雀', link: 'https://www.yuque.com/pguide/cqmua'},
-            {text: '服务状态', link: 'https://status.cqmu.online/status/cqmua'}
+            {icon: '/mua.png', text: 'MUA', link: 'http://www.mualliance.cn/'},
+            {icon: 'devicon:passport', text: 'Passport', link: 'https://p.mpsmc.cn/'},
+            {icon: '/command_block.gif', text: '服务器中心', link: 'https://server.cqmua.cn/'},
+            {icon: '/yuque.svg', text: '语雀', link: 'https://www.yuque.com/pguide/cqmua'},
+            {icon: 'icon-park:server', text: '服务状态', link: 'https://status.cqmu.online/status/cqmua'}
         ]
     }
 ])
--- docs/README.md
+++ docs/README.md
@@ -4,15 +4,12 @@ externalLinkIcon: false
 config:
   - type: hero
     full: true
-    background: /star2.jpg
+    background: /night.png
     hero:
       name: CQMUA 渝高联
       tagline: 重庆 Minecraft 高校联盟
       text: 繁星闪耀当下，方块创造未来。
       actions:
-        - theme: brand
-          text: 博客
-          link: /blog/
         - theme: brand
           text: 通行证系统
           link: https://p.mpsmc.cn/
